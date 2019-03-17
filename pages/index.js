f1d8cc079f558c8fe0f16565cb6a1813e836dfc6
Atif Mohammed <atif089@gmail.com>
2019-03-17 03:40:03 +0100

    Added Custom Head and HTML Tag

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import React from 'react';
+
 import '../scss/home.scss';
 
+import Head from 'next/head';
+
 export default () => (
-  <div className="example">
-    <p>Welcome to the custom Next.js build</p>
-  </div>
+  <React.Fragment>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Document - Custom Title</title>
+    </Head>
+    <div className="example">
+      <p>Welcome to the custom Next.js build..</p>
+    </div>
+  </React.Fragment>
 );
